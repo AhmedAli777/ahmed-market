6d877ee775a853c7fb254e58120795e8905d5eee
Ahmed Ali <ahmali777@gmail.com>
2022-08-12 17:54:52 +0200

    test

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -68,7 +68,7 @@ exports.getMyPaidProducts = catchAsync(async (req, res, next) => {
   const myProducts = await Product.find({ vendor: req.user.id });
 
   // 2) Find products with the returned IDs
-  const productIDs = myProducts.map((el) => el.id);
+  const productIDs = myProducts.map((el) => el._id);
   const products = await Booking.find({ _id: { $in: productIDs } });
 
   res.status(200).render('overview', {
