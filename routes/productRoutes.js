3f5e9c01161f8f766e11d324b0c090351a16ee34
Ahmed Ali <ahmali777@gmail.com>
2022-08-12 13:30:47 +0200

    test

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -37,16 +37,13 @@ router.use('/:productId/reviews', reviewRouter);
 router.route('/top-5-products').get(alisTopFive, getAllProducts);
 router.route('/products-stats').get(getProductsStats);
 
-router
-  .route('/')
-  .get(getAllProducts)
-  .post(protect, restrictTo('admin'), createProduct);
+router.route('/').get(getAllProducts).post(createProduct);
 router
   .route('/:id')
   .get(getProduct)
   .patch(
-    // protect,
-    // restrictTo('admin'),
+    protect,
+    restrictTo('admin'),
     uploadProductImages,
     resizeProductImages,
     updateProduct
