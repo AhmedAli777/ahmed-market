ccb6171e4d1de6aeacc831b7d8e891c0f6d48162
Ahmed Ali <ahmali777@gmail.com>
2022-08-13 20:54:49 +0200

    test

--- controllers/productController.js
+++ controllers/productController.js
@@ -32,19 +32,16 @@ exports.uploadProductImages = upload.fields([
   { name: 'images', maxCount: 3 },
 ]);
 
-// upload.single('image') req.file
-// upload.array('images', 5) req.files
-
 exports.resizeProductImages = catchAsync(async (req, res, next) => {
   if (!req.files.imageCover || !req.files.images) return next();
 
   // 1) Cover image
-  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  req.body.imageCover = `product-${req.params.id}-${Date.now()}-cover.jpeg`;
   await sharp(req.files.imageCover[0].buffer)
     .resize(2000, 1333)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/img/tours/${req.body.imageCover}`);
+    .toFile(`public/img/products/${req.body.imageCover}`);
 
   // 2) Images
   req.body.images = [];
