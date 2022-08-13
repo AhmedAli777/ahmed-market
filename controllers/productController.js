3da9924d7f97abd92ba37f1af9833e99602d22d8
Ahmed Ali <ahmali777@gmail.com>
2022-08-13 20:42:24 +0200

    test

--- controllers/productController.js
+++ controllers/productController.js
@@ -27,42 +27,37 @@ const upload = multer({
   fileFilter: multerFilter,
 });
 
-exports.uploadProductImages = catchAsync(async (req, res, next) => {
-  // if (!req.files) return next();
-  upload.fields([
-    { name: 'imageCover', maxCount: 1 },
-    { name: 'images', maxCount: 3 },
-  ]);
-  next();
-});
+exports.uploadProductImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
 
 // upload.single('image') req.file
 // upload.array('images', 5) req.files
 
 exports.resizeProductImages = catchAsync(async (req, res, next) => {
-  // if (!req.files) return next();
   if (!req.files.imageCover || !req.files.images) return next();
 
   // 1) Cover image
-  req.body.imageCover = `product-${req.params.id}-${Date.now()}-cover.jpeg`;
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
   await sharp(req.files.imageCover[0].buffer)
     .resize(2000, 1333)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/img/products/${req.body.imageCover}`);
+    .toFile(`public/img/tours/${req.body.imageCover}`);
 
   // 2) Images
   req.body.images = [];
 
   await Promise.all(
     req.files.images.map(async (file, i) => {
-      const filename = `product-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
 
       await sharp(file.buffer)
         .resize(2000, 1333)
         .toFormat('jpeg')
         .jpeg({ quality: 90 })
-        .toFile(`public/img/products/${filename}`);
+        .toFile(`public/img/tours/${filename}`);
 
       req.body.images.push(filename);
     })
